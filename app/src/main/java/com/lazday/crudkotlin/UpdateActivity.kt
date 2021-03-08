1ccfbdb6e345fe86afbf10883592297b8691123d
hiirsyad <mchirsyd.dev@gmail.com>
2021-03-08 20:13:36 +0700

    first commit

---
+++ app/src/main/java/com/lazday/crudkotlin/UpdateActivity.kt
@@ -0,0 +1,10 @@
+package com.lazday.crudkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
